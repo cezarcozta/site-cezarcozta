0d8628ce048fdeacab8054052d22a3178d58d91b
cezarcozta <cezarcozta@gmail.com>
2021-06-11 03:34:47 -0300

    fix: media queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=B612:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
         <title>cezarcozta</title>
       </Head>
       <Component {...pageProps} />
